c7bf2d129b48b1293e7a34acd1b1a087802ebed7
Cinthia-Gomez <b2jg9dbcz4@privaterelay.appleid.com>
2026-08-25 14:46:27 -0400

    Add files via upload

---
+++ Regression.R
@@ -0,0 +1,8 @@
+#Author:Cinthia Gomez, Date:08/25/2026, Purpose:Test the regression analysis
+
+#Import dummy dataset
+training_data <-mtcars
+
+#Plot the data as scatter plot 
+scatter.smooth(x=training_data$disp,y=training_data $wt,main="SpeedVSdistance")
+
