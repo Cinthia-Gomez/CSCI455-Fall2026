cb0132fed897c451fca22f294f9fae6f296e1c88
Cinthia-Gomez <b2jg9dbcz4@privaterelay.appleid.com>
2026-08-27 14:44:31 -0400

    Add files via upload

--- Regression.R
+++ Regression.R
@@ -6,3 +6,17 @@ training_data <-mtcars
 #Plot the data as scatter plot 
 scatter.smooth(x=training_data$disp,y=training_data $wt,main="SpeedVSdistance")
 
+#50% split--Underfitting
+#70% split(Assignment)
+#90% split--Overfitting(Assignment)
+
+#Create a training dataset with a 50% split
+training_dataset<-training_data[1:16,]
+test_dataset<-training_data[17:32,]
+
+#Creating regression model on mpg and wt of car
+regression_model<-lm(mpg~wt,data=training_dataset)
+
+
+
+
